49ff42ee4a9649a50d200ce6692595bd6c6e6ac2
YamileOchoa <yamileochoa137@gmail.com>
2025-12-03 14:16:24 -0500

    Subiendo  el store.js y nuevas dependencias + diseño

---
+++ src/store/store.js
@@ -0,0 +1,29 @@
+import { create } from 'zustand';
+
+export const useStore = create((set, get) => ({
+    posts: [],
+    page: 1,
+    totalPages: 1,
+    error: null,
+    fetchCharacters: async (pageParam) => {
+        set({ loading: true, error: null });
+
+        try {
+            const page = pageParam || get().page;
+            const response = await fetch(`https://jsonplaceholder.typicode.com/posts?page=${page}&limit=10`);
+            const data = await response.json();
+
+            set({
+                posts: data.items,
+                page: page,
+                totalPages: data.meta.totalPages,
+                loading: false
+            });
+        } catch (err) {
+            set({ error: err.message, loading: false });
+            console.error("Error fetching characters:", err);
+        }
+    }
+}));
+
+
